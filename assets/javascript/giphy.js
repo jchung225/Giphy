a27cc01536a0cb3dccf6f39c70390fce23ed2694
Jim Chung <jchung225@gmail.com>
2019-08-24 16:00:12 -0400

    wipped and went a new path

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,35 +1,61 @@
-$("button").on("click", function(){
-    var person = $(this).attr("data-person");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+$(document).ready(function(){
+    var Sports =[ "soccer" , "Football" , "Baseball" , "Racing" , "Volleyball" , "Tennis"]
 
-    $.ajax({
-        url:queryURL,
-        method: "GET"
-    })
-    .then(function(response){
-        var results = response.data;
-
-
-        for (var i=0; i < results.length; i++) {
-            var gifDiv = $("<div>");
-
-            var rating = results[i].rating;
-
-            var p = $("<p>").text("Rating: " + rating);
-
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
-
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
-
-            $("#gifs-appear-here").prepend(gifDiv);
+    function renderButtons(){
+        $("#sports-buttons").empty();
+        for (i=0; i < Sports.length; i++) {
+            $("#sports-buttons").append("<button class ='btn btn-success' data-sports='" + sports[i] + "'>" + sports[i] + "</button>");
         }
+    }
+
+
+    renderButtons();
+
+    $("#add-sport").on("click", function(){
+        event.preventDefault();
+        var sport = $("#sport-input").val().trim();
+        sports.push(sport);
+        renderButtons();
+        return;
+    })
+
+
+    $("button").on("click", function(){
+        var sport = $(this).attr("data-sport");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            sport + "&api_key=dc6zaTOxFJmzC&limit=10"
+            
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response){
+            var results = response.data;
+            $("#sports").empty();
+        })
+
 
 
     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
-
-var topics =[""];
