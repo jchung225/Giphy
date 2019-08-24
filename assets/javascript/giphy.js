6d7f8c3a4716044d8f4d04febda2476600561e59
Jim Chung <jchung225@gmail.com>
2019-08-24 16:48:10 -0400

    giphys are appearing and button adds more sports

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){
-    var Sports =[ "soccer" , "Football" , "Baseball" , "Racing" , "Volleyball" , "Tennis"]
+    var sports =[ "soccer" , "football" , "baseball" , "racing" , "volleyball" , "tennis"]
 
     function renderButtons(){
         $("#sports-buttons").empty();
-        for (i=0; i < Sports.length; i++) {
-            $("#sports-buttons").append("<button class ='btn btn-success' data-sports='" + sports[i] + "'>" + sports[i] + "</button>");
+        for (i=0; i < sports.length; i++) {
+            $("#sports-buttons").append("<button class='btn btn-success' data-sport='" + sports[i] + "'>" + sports[i] + "</button>");
         }
     }
-
-
     renderButtons();
 
     $("#add-sport").on("click", function(){
         event.preventDefault();
-        var sport = $("#sport-input").val().trim();
-        sports.push(sport);
+        var sportinput = $("#sport-input").val().trim();
+        sports.push(sportinput);
         renderButtons();
         return;
     })
@@ -23,7 +21,7 @@ $(document).ready(function(){
     $("button").on("click", function(){
         var sport = $(this).attr("data-sport");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            sport + "&api_key=dc6zaTOxFJmzC&limit=10"
+            sport + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10"
             
 
         $.ajax({
@@ -31,16 +29,42 @@ $(document).ready(function(){
             method: "GET"
         }).done(function(response){
             var results = response.data;
-            $("#sports").empty();
-        })
+            $("#sports").empty();   
+            for ( var i =0; i < results.length; i++){
+                var sportDiv= $("<div>");
+                var p =$("<p>").text("Rating: " + results[i].rating);
+                
+                var sportImg = $("<img>");
+                sportImg.attr("src", results[i].images.original_still.url);
+				sportImg.attr("data-still", results[i].images.original_still.url);
+				sportImg.attr("data-animate", results[i].images.original.url);
+				sportImg.attr("data-state", "still");
+				sportImg.attr("class", "gif");
+				sportDiv.append(p);
+				sportDiv.append(sportImg);
+				$("#gif-appear-here").append(sportDiv);
+
+            }
+        });
+    });
 
 
+    function imagechangeState() {
+        var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+        
+        if (state === "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        }
 
-    })
-
-
-
-
+        else if (state === "animate"){
+            $(this).attr("src", stillImage);
+            $(this).attr("src", "data-state", "still");
+        }
+    }
+    $(document).on("click", ".gif",imagechangeState);
 
 
 
